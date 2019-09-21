1bb34c0e90ae2b0a0c70801756479337e5ee9a3d
MicotoTisaki-create <micoto165320@yahoo.co.jp>
2019-09-21 11:35:17 +0900

    2019/9/21

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # このアクションを追加
+  def after_sign_in_path_for(resource)
+    "/user/#{current_user.id}"
+  end
+
+  protected
+
+  # 入力フォームからアカウント名情報をDBに保存するために追加
+  def configure_permitted_parameters
+    added_attrs = [ :username, :email, :password, :password_confirmation　]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+end
